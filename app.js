8be7ecf5c3a93823b5ea7b3d8e4a3eefe2431f59
Henok Debesay <henokdebesay@gmail.com>
2024-02-28 02:29:31 -0800

    working localhost

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const http = require("http");
-const port = 3000;
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.listen(PORT, ()=>{
+
+    console.log(`Server running on port: ${PORT}`)
+})
+
+app.get("/", (req, res)=>{
+    res.send('Hello')
+})
 
