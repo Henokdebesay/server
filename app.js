cf0f3858dab5f70eb9d0779e70a7624e864f2f07
Henok Debesay <henokdebesay@gmail.com>
2024-02-28 08:10:21 -0800

    forgot to install ejs, could have saved me time in debugging

--- app.js
+++ app.js
@@ -2,31 +2,38 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const bodyParser = require("body-parser");
-const shoppersRoutes = require('./routes/shoppers.js')
-const cartsRoutes = require('./routes/carts.js')
-const reviewsRoutes = require('./routes/reviews.js')
+const shoppersRoutes = require('./routes/shoppers.js');
+const cartsRoutes = require('./routes/carts.js');
+const reviewsRoutes = require('./routes/reviews.js');
 
 const requestLogger = (req, res, next) => {
     console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
     next(); // 
 };
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(requestLogger);
 
-app.use("/shoppers", shoppersRoutes)
-app.use("/carts", cartsRoutes)
-app.use("/reviews", reviewsRoutes)
+// Static files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
 
+// Set views
+app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.get('', (req, res) => {
+    res.render("index", { text: "this is Henok" });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port: ${PORT}`)
-})
+app.use("/shoppers", shoppersRoutes);
+app.use("/carts", cartsRoutes);
+app.use("/reviews", reviewsRoutes);
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res)=>{
-    res.send('Hello')
-})
-
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
